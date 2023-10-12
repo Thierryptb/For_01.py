fdf11e47d1858fa02694b5bf5580f4df8942a3b3
Thierryptb <146729829+Thierryptb@users.noreply.github.com>
2023-10-12 14:34:47 +0200

    Create for_01.py

---
+++ for_01.py
@@ -0,0 +1,11 @@
+# Démonstration utilisation boucle 'for' dans une liste
+
+liste = [1, 2, 3, 5, 8, 13] #Création de la liste
+for elem in liste: #On inclu la boucle 'for' pour rechercher chaques éléments dans la liste ci-dessus
+	print('nombre actuel :', elem)
+
+# Opération usuelle
+for elem in liste:
+	result = 2 * elem + 1 #On incite donc à la boucle de multiplié par 2 chaque éléments et d'y ajouter +1
+	print(result) #On écrit la variable 'résult' permettant à la boucle de l'appliquer
+
